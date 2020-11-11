b707ddeb249f603ced6568489ae38b403553712b
Mario Sánchez <Maslz980@outlook.com>
2020-11-11 15:38:19 -0600

    One signal and firebase implementation

--- App.tsx
+++ App.tsx
@@ -28,21 +28,80 @@ import {
 import SearchBar from './components/SearchBar';
 import Profile from './components/Profile';
 import Tabs from './components/Tabs';
+import OneSignal from 'react-native-onesignal';
 
 declare const global: {HermesInternal: null | {}};
 
-const App = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <SearchBar />
-        <Profile />
-      </SafeAreaView>
-      <Tabs />
-    </>
-  );
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    //Remove this method to stop OneSignal Debugging
+    OneSignal.setLogLevel(6, 0);
+
+    // Replace 'YOUR_ONESIGNAL_APP_ID' with your OneSignal App ID.
+    OneSignal.init('620c3cfc-5fe6-4d2b-b19a-ace45a72ce67', {
+      kOSSettingsKeyAutoPrompt: false,
+      kOSSettingsKeyInAppLaunchURL: false,
+      kOSSettingsKeyInFocusDisplayOption: 2,
+    });
+    OneSignal.inFocusDisplaying(2); // Controls what should happen if a notification is received while the app is open. 2 means that the notification will go directly to the device's notification center.
+
+    // The promptForPushNotifications function code will show the iOS push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step below)
+    // OneSignal.promptForPushNotificationsWithUserResponse(myiOSPromptCallback);
+
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
+  }
+
+  componentWillUnmount() {
+    OneSignal.removeEventListener('received');
+    OneSignal.removeEventListener('opened');
+    OneSignal.removeEventListener('ids');
+  }
+
+  onReceived(notification) {
+    console.log('Notification received: ', notification);
+  }
+
+  onOpened(openResult) {
+    console.log('Message: ', openResult.notification.payload.body);
+    console.log('Data: ', openResult.notification.payload.additionalData);
+    console.log('isActive: ', openResult.notification.isAppInFocus);
+    console.log('openResult: ', openResult);
+  }
+
+  onIds(device) {
+    console.log('Device info: ', device);
+  }
+
+  render() {
+    return (
+      <>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          <SearchBar />
+          <Profile />
+        </SafeAreaView>
+        <Tabs />
+      </>
+    );
+  }
+}
+
+export default App;
+// const App = () => {
+//   return (
+//     <>
+//       <StatusBar barStyle="dark-content" />
+//       <SafeAreaView>
+//         <SearchBar />
+//         <Profile />
+//       </SafeAreaView>
+//       <Tabs />
+//     </>
+//   );
+// };
 
 const styles = StyleSheet.create({
   scrollView: {
---
+++ services/Firebase.ts
@@ -0,0 +1,27 @@
+// import firebase from "firebase/app";
+// import "firebase/auth";
+// import "firebase/firestore";
+// import "firebase/functions";
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// let config = {
+// 	apiKey: process.env.REACT_APP_API_KEY,
+// 	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+// 	databaseURL: process.env.REACT_APP_DATABASE_URL,
+// 	projectId: process.env.REACT_APP_PROJECT_ID,
+// 	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+// 	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+// 	appId: process.env.REACT_APP_APP_ID,
+// };
+
+// console.log(window.location.hostname);
+// if (window.location.hostname === "localhost") {
+// 	console.log("local db");
+// 	config.databaseURL = "http://localhost:8080";
+// 	console.log(config.databaseURL);
+// }
+
+// const Firebase = firebase.initializeApp(config);
+
+// export default Firebase;
